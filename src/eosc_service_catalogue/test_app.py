c148f639bad6d6e545775a8fe15307a65122ae96
Enol Fernández <enol.fernandez@egi.eu>
2026-05-12 15:41:31 +0100

    Linting

--- src/eosc_service_catalogue/test_app.py
+++ src/eosc_service_catalogue/test_app.py
@@ -8,9 +8,10 @@ import yaml
 
 from . import model
 
+
 def test_validate_services_data():
     for svc_file in files("eosc_service_catalogue.data").iterdir():
-        if not svc_file.match("*.yaml"):
+        if not svc_file.name.endswith(".yaml"):
             continue
         print(f"Loading {svc_file}")
         svc = yaml.load(svc_file.read_text(), Loader=yaml.SafeLoader)
